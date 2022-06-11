63006e7a4c4218f243b565c0eb1e27c81f2cb7af
surya <suryakondisetti564@gmail.com>
2022-06-11 21:15:38 +0100

    java project

---
+++ src/innerClassAssisted.java
@@ -0,0 +1,21 @@
+
+public class innerClassAssisted {
+
+		// TODO Auto-generated method stub
+
+		 private String msg="Welcome to Java"; 
+		 
+		 class Inner{  
+		  void hello(){System.out.println(msg+", Let us start learning Inner Classes");}  
+		 }  
+
+
+		public static void main(String[] args) {
+
+			innerClassAssisted obj=new innerClassAssisted();
+			innerClassAssisted.Inner in=obj.new Inner();  
+			in.hello();  
+		}
+	}
+
+
